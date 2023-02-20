3532557a830414e6eec372f360ea04866872577d
Dan Pastori <dan@521dimensions.com>
2023-02-20 11:52:28 -0800

    Implemented Favicon #134

--- docs/layouts/docs.vue
+++ docs/layouts/docs.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="w-full min-h-screen bg-black">
+        <Head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+            <Link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+            <Link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+            <Link rel="manifest" href="/images/favicon/site.webmanifest"/>
+            <Link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+            <Meta name="msapplication-TileColor" content="#da532c"/>
+            <Meta name="theme-color" content="#ffffff"/>
+        </Head>
         <slot></slot>    
     </div>
 </template>
