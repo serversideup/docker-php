52d5b589ec4312a6550847cf3f38d15e18a9ccee
Dan Pastori <dan@521dimensions.com>
2023-03-06 07:25:27 -0800

    Added Guide grid to root docs page #150

---
+++ docs/components/content/Features.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/rocket-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Production-ready</span>
+            <span class="font-inter text-slate-300 text-base">Built to be performant and secure to exist on the wild web.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/heart-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Native Health Checks</span>
+            <span class="font-inter text-slate-300 text-base">Be 100% confident your application is actually running.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/lightning-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">High Performance</span>
+            <span class="font-inter text-slate-300 text-base">These images perform faster than the official PHP images.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/stars-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Customizable & Flexible</span>
+            <span class="font-inter text-slate-300 text-base">Environment variables make customizations a breeze.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/ubuntu-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built on Ubuntu 22.04</span>
+            <span class="font-inter text-slate-300 text-base">Work in a familiar and powerful operating system environment.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/shield-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built with Ondřej PHP</span>
+            <span class="font-inter text-slate-300 text-base">All packages are sourced from the highly reliable Ondřej PHP repo.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/nginx-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">NGINX + PHP-FPM</span>
+            <span class="font-inter text-slate-300 text-base">We offer NGINX + PHP-FPM support out of the box.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/logging-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Unified Logging</span>
+            <span class="font-inter text-slate-300 text-base">All logs are directed to STDOUT & STDERR for centralized output.</span>
+        </div>
+        <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
+            <img src="/images/icons/heartbeat-square.svg" class="w-11 h-11 mb-2"/>  
+            <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built with S6 Overlay</span>
+            <span class="font-inter text-slate-300 text-base">An extremely intelligent and powerful method to running PHP.</span>
+        </div>
+    </div>
+</template>
