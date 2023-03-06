55360f87673628eeac218d5fa5ecfa937ab882d7
Jay Rogers <jay@521dimensions.com>
2023-03-06 14:21:53 -0600

    Adjusted middleware

--- docs/middleware/directory.ts
+++ docs/middleware/directory.ts
@@ -1,10 +1,13 @@
 export default defineNuxtRouteMiddleware(( to, from ) => {
     switch( to.path ){
-        case '/docs/getting-started':
+        case '/docs/getting-started/':
             return navigateTo( to.path+'/these-images-vs-others', { replace: true } );
         break;
-        case '/docs/guide':
+        case '/docs/guide/':
             return navigateTo( to.path+'/choosing-the-right-image', { replace: true } );
         break;
+        case '/docs/reference/':
+            return navigateTo( to.path+'/environment-variable-specification', { replace: true } );
+        break;
     }
-});
+})
