cfbf69cbb45d4e35031e32aa8f0083e4d3a8a1ff
Dan Pastori <dan@521dimensions.com>
2023-03-06 12:28:12 -0800

    Cleaned up redirects

--- docs/middleware/directory.ts
+++ docs/middleware/directory.ts
@@ -1,16 +1,13 @@
 export default defineNuxtRouteMiddleware(( to, from ) => {
     let redirectPath = to.path.endsWith('/') ? to.path.slice(0, -1) : to.path;
 
-    switch( to.path ){
-        case '/docs/getting-started/':
+    switch( redirectPath ){
         case '/docs/getting-started':
             return navigateTo( redirectPath+'/these-images-vs-others', { replace: true } );
         break;
-        case '/docs/guide/':
         case '/docs/guide':
             return navigateTo( redirectPath+'/choosing-the-right-image', { replace: true } );
         break;
-        case '/docs/reference/':
         case '/docs/reference':
             return navigateTo( redirectPath+'/environment-variable-specification', { replace: true } );
         break;
