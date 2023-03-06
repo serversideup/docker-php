469062094cb428b7ac356d54b48f2746bc42162f
Dan Pastori <dan@521dimensions.com>
2023-03-06 12:25:51 -0800

    Redirects based off of back slashes

--- docs/middleware/directory.ts
+++ docs/middleware/directory.ts
@@ -1,13 +1,18 @@
 export default defineNuxtRouteMiddleware(( to, from ) => {
+    let redirectPath = to.path.endsWith('/') ? to.path.slice(0, -1) : to.path;
+
     switch( to.path ){
         case '/docs/getting-started/':
-            return navigateTo( to.path+'/these-images-vs-others', { replace: true } );
+        case '/docs/getting-started':
+            return navigateTo( redirectPath+'/these-images-vs-others', { replace: true } );
         break;
         case '/docs/guide/':
-            return navigateTo( to.path+'/choosing-the-right-image', { replace: true } );
+        case '/docs/guide':
+            return navigateTo( redirectPath+'/choosing-the-right-image', { replace: true } );
         break;
         case '/docs/reference/':
-            return navigateTo( to.path+'/environment-variable-specification', { replace: true } );
+        case '/docs/reference':
+            return navigateTo( redirectPath+'/environment-variable-specification', { replace: true } );
         break;
     }
 })
