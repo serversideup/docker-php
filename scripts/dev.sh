9981c42076f846a1f8359876c6ed8b6e67041b57
Jay Rogers <jay@521dimensions.com>
2023-10-11 10:40:22 -0500

    Improved development script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 #exit on error
-set -oue pipefail
+set -oex pipefail
+
+##########################################################################
+# Usage: build.sh --variation <variation> --version <version> --os <os>
+##########################################################################
+# This script is used to build a Docker image for a specific PHP version
+# and variation. It is intended to be used for local development. You may
+# also change the DOCKER_REPOSITORY environment variable or pass other
+# arguments to the docker build command, like "--no-cache".
 
 ##########################
 # Environment Settings
@@ -11,10 +19,11 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 
-PHP_BUILD_VERSION="${PHP_BUILD_VERSION:-"$1"}"
-PHP_BUILD_VARIATION="${PHP_BUILD_VARIATION:-"$2"}"
-PHP_BUILD_BASE_OS="${PHP_BUILD_BASE_OS:-"$3"}"
+PHP_BUILD_VERSION=""
+PHP_BUILD_VARIATION=""
+PHP_BUILD_BASE_OS=""
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-"serversideup/php-pro"}"
+DOCKER_ADDITIONAL_BUILD_ARGS=()
 
 # UI Colors
 function ui_set_yellow {
@@ -58,6 +67,7 @@ check_vars() {
 build_docker_image() {
   echo_color_message yellow "🐳 Building Docker Image: $DOCKER_REPOSITORY:$PHP_BUILD_VERSION-$PHP_BUILD_VARIATION-$PHP_BUILD_BASE_OS"
   docker build \
+    ${DOCKER_ADDITIONAL_BUILD_ARGS[@]} \
     --build-arg PHP_VARIATION="$PHP_BUILD_VARIATION" \
     --build-arg PHP_VERSION="$PHP_BUILD_VERSION" \
     --build-arg BASE_OS_VERSION="$PHP_BUILD_BASE_OS" \
@@ -69,10 +79,45 @@ build_docker_image() {
 
 ##########################
 # Main script starts here
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --variation)
+        PHP_BUILD_VARIATION="$2"
+        shift 2
+        ;;
+        --os)
+        PHP_BUILD_BASE_OS="$2"
+        shift 2
+        ;;
+        --version)
+        PHP_BUILD_VERSION="$2"
+        shift 2
+        ;;
+        --*)
+        # If there's a next argument and it starts with '--', treat the current argument as standalone.
+        # Otherwise, pair the current argument with the next.
+        if [[ $# -gt 1 && $2 =~ ^-- ]]; then
+            DOCKER_ADDITIONAL_BUILD_ARGS+=("$1")
+            shift
+        else
+            DOCKER_ADDITIONAL_BUILD_ARGS+=("$1")
+            [[ $# -gt 1 ]] && DOCKER_ADDITIONAL_BUILD_ARGS+=("$2") && shift
+            shift
+        fi
+        ;;
+        *)
+        # Skip the argument if not recognized
+        shift
+        ;;
+    esac
+done
+
+
 check_vars \
   "🚨 Required variables not set" \
   PHP_BUILD_VARIATION \
   PHP_BUILD_VERSION \
-  PHP_BUILD_BASE_OS \
+  PHP_BUILD_BASE_OS
 
 build_docker_image
