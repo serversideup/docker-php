43b4adf838fedd89ff0f4cda17b6f6fc2891a185
Jay Rogers <jay@521dimensions.com>
2023-11-03 12:47:50 -0500

    Updated landing page

--- docs/components/content/MarketingGrid.vue
+++ docs/components/content/MarketingGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="mx-auto max-w-[940px] flex flex-col px-6 py-8 lg:py-16">
         <h2 class="font-inter text-white font-bold text-[28px] leading-[28px] text-center mb-16 lg:text-5xl lg:leading-[48px]">
-            These are <span class="text-pink-500">very different</span> from other PHP Docker Images.
+            These images <span class="text-pink-500">give a lot more</span> than other PHP Docker Images.
         </h2>
 
         <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
@@ -18,7 +18,7 @@
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
                 <img src="/images/icons/lightning-square.svg" class="w-11 h-11 mb-2"/>  
                 <span class="font-inter text-lg text-slate-50 font-bold mb-2">High Performance</span>
-                <span class="font-inter text-slate-300 text-base">These images perform faster than the official PHP images.</span>
+                <span class="font-inter text-slate-300 text-base">Get the easiest experience for fine tuning performance.</span>
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
                 <img src="/images/icons/stars-square.svg" class="w-11 h-11 mb-2"/>  
@@ -26,19 +26,19 @@
                 <span class="font-inter text-slate-300 text-base">Environment variables make customizations a breeze.</span>
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
-                <img src="/images/icons/ubuntu-square.svg" class="w-11 h-11 mb-2"/>  
-                <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built on Ubuntu 22.04</span>
-                <span class="font-inter text-slate-300 text-base">Work in a familiar and powerful operating system environment.</span>
+                <img src="/images/icons/cloudflare-square.svg" class="w-11 h-11 mb-2"/>  
+                <span class="font-inter text-lg text-slate-50 font-bold mb-2">Native CloudFlare Support</span>
+                <span class="font-inter text-slate-300 text-base">Get real IP addresses from visitors from trusted proxies.</span>
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
-                <img src="/images/icons/shield-square.svg" class="w-11 h-11 mb-2"/>  
-                <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built with Ondřej PHP</span>
-                <span class="font-inter text-slate-300 text-base">All packages are sourced from the highly reliable Ondřej PHP repo.</span>
+                <img src="/images/icons/php-square.svg" class="w-11 h-11 mb-2"/>  
+                <span class="font-inter text-lg text-slate-50 font-bold mb-2">Based on official PHP</span>
+                <span class="font-inter text-slate-300 text-base">Upgrade from the official PHP docker images with confidence.</span>
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
                 <img src="/images/icons/nginx-square.svg" class="w-11 h-11 mb-2"/>  
-                <span class="font-inter text-lg text-slate-50 font-bold mb-2">NGINX + PHP-FPM</span>
-                <span class="font-inter text-slate-300 text-base">We offer NGINX + PHP-FPM support out of the box.</span>
+                <span class="font-inter text-lg text-slate-50 font-bold mb-2">NGINX Unit</span>
+                <span class="font-inter text-slate-300 text-base">We offer NGINX Unit support out of the box.</span>
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
                 <img src="/images/icons/logging-square.svg" class="w-11 h-11 mb-2"/>  
@@ -47,8 +47,8 @@
             </div>
             <div class="p-4 bg-gray-900 rounded-lg flex flex-col">
                 <img src="/images/icons/heartbeat-square.svg" class="w-11 h-11 mb-2"/>  
-                <span class="font-inter text-lg text-slate-50 font-bold mb-2">Built with S6 Overlay</span>
-                <span class="font-inter text-slate-300 text-base">An extremely intelligent and powerful method to running PHP.</span>
+                <span class="font-inter text-lg text-slate-50 font-bold mb-2">FPM + S6 Overlay</span>
+                <span class="font-inter text-slate-300 text-base">Our FPM-Apache & FPM-NGINX images use this intelligent init system.</span>
             </div>
         </div>
     </section>
