fcfed5158467fc4f45fbaacec1a3f5fa651e41d9
Jay Rogers <jay@521dimensions.com>
2023-11-27 15:18:45 -0600

    Updated Laravel automations

--- src/common/etc/entrypoint.d/50-laravel-automations.sh
+++ src/common/etc/entrypoint.d/50-laravel-automations.sh
@@ -6,33 +6,63 @@ fi
 # Exit on error
 set -e
 
-echo "Laravel automations"
+# Check to see if an Artisan file exists and assume it means Laravel is configured.
+if [ -f $APP_BASE_DIR/artisan ] && [ ${AUTORUN_ENABLED:="true"} == "true" ]; then
+        echo "Checking for Laravel automations..."
 
-# # Check to see if an Artisan file exists and assume it means Laravel is configured.
-# if [ -f $WEB_APP_DIRECTORY/artisan ] && [ ${AUTORUN_ENABLED:="true"} == "true" ]; then
-#         echo "Checking for Laravel automations..."
+        ############################################################################
+        # artisan config:cache
+        ############################################################################
+        if [ ${AUTORUN_LARAVEL_CONFIG_CACHE:="true"} == "true" ]; then
+            echo "🚀 Caching Laravel config..."
+            php $APP_BASE_DIR/artisan config:cache
+        fi
 
-#         ############################################################################
-#         # Automated database migrations
-#         ############################################################################
-#         if [ ${AUTORUN_LARAVEL_MIGRATION:="false"} == "true" ]; then
-#             echo "🚀 Running migrations..."
-#             php $WEB_APP_DIRECTORY/artisan migrate --force --isolated
-#         fi
+        ############################################################################
+        # artisan route:cache
+        ############################################################################
+        if [ ${AUTORUN_LARAVEL_ROUTE_CACHE:="true"} == "true" ]; then
+            echo "🚀 Caching Laravel routes..."
+            php $APP_BASE_DIR/artisan route:cache
+        fi
 
-#         ############################################################################
-#         # Automated storage linking
-#         ############################################################################
-#         if [ ${AUTORUN_LARAVEL_STORAGE_LINK:="true"} == "true" ]; then
-#             if [ -d $WEB_APP_DIRECTORY/public/storage ]; then
-#                 echo "✅ Storage already linked..."
-#             else
-#                 echo "🔐 Linking the storage..."
-#                 php $WEB_APP_DIRECTORY/artisan storage:link
-#             fi
-#         fi
-# else
-#     echo "👉 Skipping Laravel automations because we could not detect a Laravel install or it was specifically disabled..."
-# fi
+        ############################################################################
+        # artisan view:cache
+        ############################################################################
+        if [ ${AUTORUN_LARAVEL_VIEW_CACHE:="true"} == "true" ]; then
+            echo "🚀 Caching Laravel views..."
+            php $APP_BASE_DIR/artisan view:cache
+        fi
 
-# exit 0
+        ############################################################################
+        # artisan event:cache
+        ###########################################################################
+        if [ ${AUTORUN_LARAVEL_EVENT_CACHE:="true"} == "true" ]; then
+            echo "🚀 Caching Laravel events..."
+            php $APP_BASE_DIR/artisan event:cache
+        fi
+        
+        ############################################################################
+        # artisan migrate
+        ############################################################################
+        if [ ${AUTORUN_LARAVEL_MIGRATION:="false"} == "true" ]; then
+            echo "🚀 Running migrations..."
+            php $APP_BASE_DIR/artisan migrate --force --isolated
+        fi
+
+        ############################################################################
+        # Automated storage linking
+        ############################################################################
+        if [ ${AUTORUN_LARAVEL_STORAGE_LINK:="true"} == "true" ]; then
+            if [ -d $APP_BASE_DIR/public/storage ]; then
+                echo "✅ Storage already linked..."
+            else
+                echo "🔐 Linking the storage..."
+                php $APP_BASE_DIR/artisan storage:link
+            fi
+        fi
+else
+    echo "👉 Skipping Laravel automations because we could not detect a Laravel install or it was specifically disabled..."
+fi
+
+exit 0
