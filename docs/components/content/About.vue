a750eac07f8cbe20516853bcf19482e8003ac87a
Dan Pastori <dan@521dimensions.com>
2023-12-05 09:20:02 -0800

    Adjusted w and h of image

--- docs/components/content/About.vue
+++ docs/components/content/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center justify-center space-x-4">
-        <div class="not-prose w-[150px] h-[150px] flex flex-col items-center justify-center">
-            <img src="https://serversideup.net/wp-content/uploads/2023/08/dan.jpg" alt="Dan Pastori" class="rounded-full w-full h-full">
+        <div class="not-prose flex flex-col items-center justify-center">
+            <img src="https://serversideup.net/wp-content/uploads/2023/08/dan.jpg" alt="Dan Pastori" class="rounded-full w-[150px] h-[150px]">
             <div class="w-full flex items-center justify-center mt-5 space-x-5">
                 <a href="https://twitter.com/danpastori" target="_blank">
                     <img src="/images/logos/x-logo.svg" class="w-5"/>
@@ -11,8 +11,8 @@
                 </a>
             </div>
         </div>
-        <div class="not-prose w-[150px] h-[150px] flex flex-col items-center justify-center">
-            <img src="https://serversideup.net/wp-content/uploads/2023/08/jay.jpg" alt="Jay Rogers" class="rounded-full w-full h-full">
+        <div class="not-prose flex flex-col items-center justify-center">
+            <img src="https://serversideup.net/wp-content/uploads/2023/08/jay.jpg" alt="Jay Rogers" class="rounded-full w-[150px] h-[150px]">
             <div class="w-full flex items-center justify-center mt-5 space-x-5">
                 <a href="https://twitter.com/jaydrogers" target="_blank">
                     <img src="/images/logos/x-logo.svg" class="w-5"/>
