3c508b0c16350d5be3b6313d727d0eb900423b8b
test521dimensions <94475950+test521dimensions@users.noreply.github.com>
2024-04-29 16:45:03 -0500

    Test change

--- src/common/etc/entrypoint.d/0-container-info.sh
+++ src/common/etc/entrypoint.d/0-container-info.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ "$LOG_OUTPUT_LEVEL" != "off" ] && [ "$DISABLE_DEFAULT_CONFIG" = false ]; then
+echo "This is a test change"
 echo '
 --------------------------------------------------------------------
  ____                             ____  _     _        _   _
