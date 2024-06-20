7a6f6eaf7eee04da2b70e0301be9bf2b9a6520ce
test521dimensions <94475950+test521dimensions@users.noreply.github.com>
2024-06-20 09:59:15 -0500

    Test PR workflow

--- src/common/etc/entrypoint.d/0-container-info.sh
+++ src/common/etc/entrypoint.d/0-container-info.sh
@@ -16,7 +16,7 @@ echo '
 |____/ \___|_|    \_/ \___|_|    |____/|_|\__,_|\___|  \___/| .__/
                                                             |_|
 
-Brought to you by serversideup.net
+TEST #1
 --------------------------------------------------------------------'
 
 PHP_OPCACHE_STATUS=$(php -r 'echo ini_get("opcache.enable");')
