14113bfe99e868964e98802ccd9e38510839fefc
Jay Rogers <jaydrogers@users.noreply.serversideup.net>
2025-10-21 17:06:06 -0500

    Added favicon, Fixes #580

--- docs/app/app.vue
+++ docs/app/app.vue
@@ -42,7 +42,11 @@ useHead({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [
-        { rel: 'icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
     ],
     htmlAttrs: {
         lang: 'en',
